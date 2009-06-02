50b90f5b39c86c0d9bcf1db2515f15486d0e69dc
Ben Spaulding <ben@benspaulding.com>
2009-06-01 20:47:59 -0500

    Prepared packaging files for release.

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
+import os
 from distutils.core import setup
 
-setup(name='faq',
-      version='0.1',
-      description='Frequently Asked Questions (FAQ) management app for Django applications.',
-      author='Ben Spaulding',
-      author_email='ben@benspaulding.com',
-      url='http://github.com/benspaulding/django-faq/',
-      packages=['faq'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Content Management'],
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='Django FAQ',
+    version='0.8.0',
+    description='Frequently Asked Question (FAQ) management for Django apps.',
+    url='http://github.com/benspaulding/django-faq/',
+    author='Ben Spaulding',
+    author_email='ben@benspaulding.com',
+    license='BSD',
+    download_url='http://github.com/benspaulding/django-faq/tarball/v0.8.0',
+    long_description = read('README'),
+    packages = ['faq'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Internet :: WWW/HTTP :: Site Management'],
 )
