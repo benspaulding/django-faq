22deeb3c61d302597e1133e04bf617baffb73ee9
Ben Spaulding <ben@benspaulding.com>
2009-06-01 21:39:58 -0500

    Renamed package to follow Django reusable-app convention.

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='Django FAQ',
-    version='0.8.0',
+    name='django-faq',
+    version='0.8.0.1',
     description='Frequently Asked Question (FAQ) management for Django apps.',
     url='http://github.com/benspaulding/django-faq/',
     author='Ben Spaulding',
     author_email='ben@benspaulding.com',
     license='BSD',
-    download_url='http://github.com/benspaulding/django-faq/tarball/v0.8.0',
+    download_url='http://github.com/benspaulding/django-faq/tarball/v0.8.0.1',
     long_description = read('README'),
     packages = ['faq'],
     classifiers=['Development Status :: 4 - Beta',
