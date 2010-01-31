de393b864dce2766afd7b9ca20580ef694425e07
benspaulding <ben@benspaulding.com>
2010-01-31 04:09:19 -0600

    Removed unused signals.
    This may be needed someday, but not now.

--- faq/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch import Signal
-
-
-published = Signal(providing_args=('instance', 'created'))
-unpublished = Signal(providing_args=('instance', 'deleted'))
-sites_changed = Signal(providing_args=('instance', ))
