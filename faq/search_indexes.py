74e93b60d1601bda9580350c45c1e0235fdb704c
Ben Spaulding <ben@benspaulding.us>
2011-04-19 12:12:54 -0500

    Removed title index field from search indexes.
    The title is already being indexed in the text
    document.

--- faq/search_indexes.py
+++ faq/search_indexes.py
@@ -5,19 +5,18 @@ from faq.models import Topic, Question
 
 
 class FAQIndexBase(indexes.SearchIndex):
+
     text = indexes.CharField(document=True, use_template=True)
     url = indexes.CharField(model_attr='get_absolute_url', indexed=False)
 
 
 class TopicIndex(FAQIndexBase):
-    title = indexes.CharField(model_attr='title', indexed=True)
 
     def get_queryset(self):
         return Topic.objects.published()
 
 
 class QuestionIndex(FAQIndexBase):
-    title = indexes.CharField(model_attr='question', indexed=True)
 
     def get_queryset(self):
         return Question.objects.published()
