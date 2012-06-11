e438272efef34617c2ec2ac21c6485de489cafa1
Ben Spaulding <ben@benspaulding.us>
2012-06-11 15:53:07 -0500

    Change to the quasi-standard __version__ for providing package version.
    Also use a reliable way to get version into setup.py. Depending on what
    happens in the package __init__.py, you can have import issues when
    importing into the setup.py, when setup is not complete. This way is
    slightly more complex, but avoids the import issues. Thanks to carljm
    for the example.

--- faq/__init__.py
+++ faq/__init__.py
@@ -8,7 +8,7 @@ A Frequently Asked Question (FAQ) management application for Django apps.
 from django.utils.translation import ugettext_lazy as _
 
 
-VERSION = '0.8.2'
+__version__ = '0.8.2'
 
 # Mark the app_label for translation.
 _(u'faq')
--- setup.py
+++ setup.py
@@ -4,18 +4,30 @@ import os
 from distutils.core import setup
 
 
+here = os.path.dirname(__file__)
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+# Function borrowed from carljm.
+def get_version():
+    fh = open(os.path.join(here, "faq", "__init__.py"))
+    try:
+        for line in fh.readlines():
+            if line.startswith("__version__ ="):
+                return line.split("=")[1].strip().strip("'")
+    finally:
+        fh.close()
+
 setup(
     name='django-faq',
-    version=VERSION,
+    version=get_version(),
     description='Frequently Asked Question (FAQ) management for Django apps.',
     url='https://github.com/benspaulding/django-faq/',
     author='Ben Spaulding',
     author_email='ben@benspaulding.us',
     license='BSD',
-    download_url='http://github.com/benspaulding/django-faq/tarball/v%s' % VERSION,
+    download_url='http://github.com/benspaulding/django-faq/tarball/v%s' % get_version(),
     long_description = read('README.rst'),
     packages = ['faq', 'faq.urls', 'faq.views'],
     package_data = {'faq': ['locale/*/LC_MESSAGES/*',
